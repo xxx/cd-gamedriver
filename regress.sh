8a05630ec643efd1465a92b063e8c3f95fbb4502
mpd <mpd@jesters-court.net>
2019-08-04 17:10:26 -0700

    Add check target for cmake, and update the regression script so the driver directory is configurable via an env var

--- regress.sh
+++ regress.sh
@@ -1,13 +1,15 @@
 #! /bin/sh
 
+DIR="${DIR:-.}"
+
 echo Note that there is supposed to be errors in the following
 echo output. There should, however, be a statement at the end
 echo stating all regression tests has passed.
 echo
 echo Running regression tests...
-(./driver -m`pwd`/regress 31789 -u-1 -p-1 2>&1) | tee `pwd`/regress/regress.output
+("$DIR"/driver -m"$(pwd)"/regress 31789 -u-1 -p-1 2>&1) | tee "$(pwd)"/regress/regress.output
 
-diff `pwd`/regress/expected.output `pwd`/regress/regress.output
+diff "$(pwd)"/regress/expected.output "$(pwd)"/regress/regress.output
 
 if [ $? != 0 ]; then
     echo
@@ -17,5 +19,5 @@ fi
 
 echo
 echo INFO: All regression tests passed.
-rm -f `pwd`/regress/regress.output
+rm -f "$(pwd)"/regress/regress.output
 exit 0
